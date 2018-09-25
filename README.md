20d0ea119257fe5e30ecc7bbdcb015e79ec83f6d
Your Name <you@example.com>
2018-09-26 00:48:24 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # electronjs-todo-list
+
+[![Build Status][ico-travis]][link-travis]
+[![License][ico-license]][link-license]
+
+TODO-List based on Electron.js.
+
+## ⚡ Installation
+
+Via GIT
+
+```bash
+$ git clone https://github.com/voyula/electronjs-todo-list.git
+$ cd electronjs-todo-list
+$ npm install
+```
+
+## 🐣 Running
+
+```bash
+npm run start
+```
+
+### 📜 Standards
+
+- [Semantic Versioning 2.0.0](https://semver.org/)
+
+### 🛠 Contributing
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) for details.
+
+### 🎙 Credits
+
+- [voyula](https://github.com/voyula)
+- [All Contributors](../../contributors)
+
+### 📌 License
+
+Licensed under the MIT License. See [License File](LICENSE.md) for more information.
+
+[ico-travis]: https://img.shields.io/travis/voyula/electronjs-todo-list/master.svg?longCache=true&style=flat-square
+
+[ico-license]: https://img.shields.io/github/license/voyula/electronjs-todo-list.svg?longCache=true&style=flat-square
+
+
+[link-travis]: https://travis-ci.org/voyula/electronjs-todo-list
+
+[link-license]: LICENSE.md
--- main.js
+++ main.js
@@ -10,15 +10,14 @@ let mainWindow,
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    backgroundColor: 'lightgray',
     width: 800,
     height: 600
   })
 
   mainWindow.loadFile('windows/main.html')
 
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu)
 
   mainWindow.on('closed', function() {
     mainWindow = null
@@ -26,9 +25,9 @@ app.on('ready', function() {
 })
 
 ipcMain.on('item:add', function(e, item) {
-  mainWindow.webContents.send('item:add', item);
-  addWindow.close();
-  addWindow = null;
+  mainWindow.webContents.send('item:add', item)
+  addWindow.close()
+  addWindow = null
 });
 
 const mainMenuTemplate = [{
@@ -43,28 +42,28 @@ const mainMenuTemplate = [{
         })
         addWindow.loadFile('windows/add.html')
         addWindow.on('close', function() {
-          addWindow = null;
+          addWindow = null
         })
       }
     },
     {
       label: 'Clear Items',
       click() {
-        mainWindow.webContents.send('item:clear');
+        mainWindow.webContents.send('item:clear')
       }
     },
     {
       label: 'Quit',
       accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
       click() {
-        app.quit();
+        app.quit()
       }
     }
   ]
 }];
 
 if (process.platform == 'darwin') {
-  mainMenuTemplate.unshift({});
+  mainMenuTemplate.unshift({})
 }
 
 app.on('window-all-closed', function() {
